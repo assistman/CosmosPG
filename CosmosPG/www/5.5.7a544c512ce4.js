1f7bcbcb8cca1ab53f562ae35332b63c501e1480
Stanislav <Stanislav.Razbiegin@kstt.ua>
2019-10-04 01:30:26 +0300

    fb fixed

---
+++ CosmosPG/www/5.5.7a544c512ce4.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[5],{
+
+/***/ "./src/js/modules/services/news/components/NewsEmpty.js":
+/*!**************************************************************!*\
+  !*** ./src/js/modules/services/news/components/NewsEmpty.js ***!
+  \**************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (function (_ref) {\n    var text = _ref.text;\n\n    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\n        \"div\",\n        { className: \"pane pane__column news__empty\" },\n        react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\n            \"div\",\n            { className: \"pane flex-valign-center flex-align-center \" },\n            react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\"div\", { className: \"icon-empty-news\" })\n        ),\n        react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\n            \"div\",\n            { className: \"text\" },\n            text\n        )\n    );\n});\n\n//# sourceURL=webpack:///./src/js/modules/services/news/components/NewsEmpty.js?");
+
+/***/ })
+
+}]);
